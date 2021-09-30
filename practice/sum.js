28c34250222229a16e2ee54470c7a8b35d6410c1
Samyo <love51213@gmail.com>
2021-09-30 17:21:25 +0800

    更新sum.js

--- practice/sum.js
+++ practice/sum.js
@@ -4,7 +4,7 @@ function sum(param) {
   //TODO: 請從1+2+3+...+param
   //param: 6 =>1+2+3+4+5+6;
   let n = 0;
-  for (i = 0; i <= param; i++) {
+  for (let i = 0; i <= param; i++) {
     n = n + i;
   }
   // console.log(n);
