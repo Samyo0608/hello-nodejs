61d9caf8d10015c4ad10c225bbbb9cf6e3d5cdb7
Samyo0608 <love51213@gmail.com>
2021-10-17 16:59:02 +0800

    1017更新，dotenv版本+await

--- crawler/app-await.js
+++ crawler/app-await.js
@@ -3,8 +3,18 @@ const axios = require("axios");
 // "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=" + format + "&date=" + today + "&stockNo=" + stockCode
 // `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=${format}&date=${today}&stockNo=${stockCode}`
 
+
+// 安裝 npm moment 控制日期 <--好用
+// const momnet = require("moment");
+// let today = moment().format("YYYYMMDD");
+
+// 控制股票代碼
+// const fs = require("fs");
+// 
+
+
 let stockCode = "2330";
-let today = "20211017";
+let today = "20211011"
 let format = "json";
 
 async function getData() {
