3e0fdfed88ca17c96cf4a7b82313c5e9feb87fab
Samyo0608 <love51213@gmail.com>
2021-10-17 11:08:24 +0800

    await版本更新排版

--- crawler/app-await.js
+++ crawler/app-await.js
@@ -7,22 +7,22 @@ let stockCode = "2330";
 let today = "20211017";
 let format = "json";
 
-async function getData(){
-
-    try{
-        const testAwait = await axios.get
-          ("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-            params: {
-              response: format,
-              date: today,
-              stockNo: stockCode,
-            },
-          });
-          console.log("await成功", testAwait.data);
-    }catch(e){
-        console.log("發生錯誤", e);
-    }
+async function getData() {
+  try {
+    const testAwait = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          response: format,
+          date: today,
+          stockNo: stockCode,
+        },
+      }
+    );
+    console.log("await成功", testAwait.data);
+  } catch (e) {
+    console.log("發生錯誤", e);
+  }
 }
 
 getData();
-
